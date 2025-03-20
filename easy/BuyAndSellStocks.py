116b0fc273a4543de469e9c6f30eb1ad98024c3f
Tuscy <gingertuscs@gmail.com>
2025-03-20 13:38:10 +0000

    Completed CountingBits

--- easy/BuyAndSellStocks.py
+++ easy/BuyAndSellStocks.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
-        profit = 0
+        buy = prices[0] #Set the buy value to the first item
+        profit = 0 
 
-        for i in range(1, len(prices)):
-            if prices[i] < buy:
+        for i in range(1, len(prices)): #For the prices not including the set buy value
+            if prices[i] < buy: #If the new price is lower than the buy set that to the new buy value
                 buy = prices[i]
-            elif profit < (prices[i] - buy):
-                profit = prices[i] - buy
+            elif profit < (prices[i] - buy): #Otherwise check how much profit would be made
+                profit = prices[i] - buy #If higher than the current highest set new value
         return profit
---
+++ easy/CountingBits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bits = [0] #Sets initial bit to 0
+
+        for i in range(1, n+1):
+            binary = bin(i)[2:] #Get the binary value without the 0b start
+            count = 0 #1s Counter
+            for x in binary: #For every 1 in the binary value add 1
+                if int(x) == 1: 
+                    count += 1
+            bits.append(count) #Append to list
+
+        return bits
+    
